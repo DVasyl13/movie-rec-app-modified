cc6e7a64431c6323fe5d7a3cd9370f3b7d105442
DVasyl13 <vasylldruchok6119@gmail.com>
2023-06-20 19:15:49 +0300

    slider now sliding

--- front-end/src/components/movie-slider/Slider.tsx
+++ front-end/src/components/movie-slider/Slider.tsx
@@ -2,22 +2,13 @@ import SliderElement from "./SliderElement";
 import './Slider.css';
 import React from "react";
 
-
 type SliderProps = {
     sliderIndex: number;
 };
-
 const Slider = (props: SliderProps) => {
-    const sliderRef = React.useRef<HTMLDivElement>(null);
-
-    React.useEffect(() => {
-        if (sliderRef.current) {
-            sliderRef.current.style.transform = `translateX(calc(${props.sliderIndex} * -100%))`;
-        }
-    }, [props.sliderIndex]);
 
     return (
-        <div className="slider">
+        <div className="slider" style={{ transform: `translateX(calc(${props.sliderIndex} * -100%))` }}>
             <SliderElement src='https://via.placeholder.com/220/00FF00?text=1'></SliderElement>
             <SliderElement src='https://via.placeholder.com/220/00FF00?text=2'></SliderElement>
             <SliderElement src='https://via.placeholder.com/220/00FF00?text=3'></SliderElement>
