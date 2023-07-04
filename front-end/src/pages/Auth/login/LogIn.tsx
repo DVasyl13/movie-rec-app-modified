7ae66667a9127f430e6e8ccc2c906e8f5b4f733f
DVasyl13 <vasylldruchok6119@gmail.com>
2023-07-04 18:48:00 +0300

    now jwt works on frontend side

--- front-end/src/pages/Auth/login/LogIn.tsx
+++ front-end/src/pages/Auth/login/LogIn.tsx
@@ -1,41 +1,57 @@
 import '../Auth.css';
 import '../AuthButtons.css';
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import Button from "../../../components/Button";
 import LogRegWithSocial from "../LogRegWithSocial";
 import {useSignIn} from "react-auth-kit";
-import {useState} from "react";
+import React, {useState} from "react";
 import {AuthApiResponse} from "../../../types/interfaces/AuthApiResponse";
+import {useFormik} from "formik";
+import {LoginRequest} from "../../../types/interfaces/LoginRequest";
 
 const LogIn = () => {
     const [error, setError] = useState("");
     const signIn = useSignIn();
+    const navigator = useNavigate();
 
-    //TODO: auth
-    const onSubmit = async (values: any) => {
+    const onSubmit = async (values: LoginRequest) => {
         console.log("Values: ", values);
         setError("");
 
         try {
-            const response: Response = await fetch("http://localhost:8080/api/v1/auth");
+            const response: Response = await fetch("http://localhost:8080/api/v1/auth/authenticate", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            });
             const responseData: AuthApiResponse = await response.json();
-
+            console.log(responseData);
             signIn({
                 token: responseData.token,
-                expiresIn: 3600,
+                expiresIn: 3600 * 24,
                 tokenType: "Bearer",
-                authState: { email: values.email },
+                authState: {email: values.email},
             });
-
+            navigator('/account');
         } catch (err) {
             console.log("Error: ", err);
         }
     };
 
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        onSubmit,
+    });
+
 
     return (
         <div className="login-container">
-           <LogRegWithSocial/>
+            <LogRegWithSocial/>
             <div className="right-auth-container">
                 <div className="auth-form title-form">
                     <p className='title-medium bottom-padding-mid'>Login</p>
@@ -43,16 +59,28 @@ const LogIn = () => {
                         Your Account</NavLink> it takes less than minute</p>
                 </div>
 
-                <div className="auth-form inputs-form">
-                    <input placeholder="Username"/>
-                    <p hidden></p>
-                    <input placeholder="Password"/>
-                    <p hidden></p>
-                </div>
-                <div className="auth-form button-form">
-                    <Button class="btn btn-sizing btn-big" text="Submit" onClick={() => {}}/>
+                <form className="auth-form inputs-form"
+                      onSubmit={formik.handleSubmit}>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Enter email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                    />
+                    <input
+                        id="password"
+                        type="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        placeholder="Enter password"
+                    />
+                    <div className="auth-form">
+                        <Button class="btn btn-sizing btn-big" text="Submit" onClick={() => {
+                        }}/>
+                    </div>
                     <NavLink className="link" to="/password-recovery">Forget your password?</NavLink>
-                </div>
+                </form>
             </div>
         </div>
     );
--- front-end/src/pages/Auth/signin/SignIn.tsx
+++ front-end/src/pages/Auth/signin/SignIn.tsx
@@ -1,10 +1,60 @@
 import '../Auth.css';
 import '../AuthButtons.css';
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
+
 import Button from "../../../components/Button";
 import LogRegWithSocial from "../LogRegWithSocial";
+import {useFormik} from "formik";
+import {RegistrationRequest} from "../../../types/interfaces/RegistrationRequest";
+import {AuthApiResponse} from "../../../types/interfaces/AuthApiResponse";
+import React, {useState} from "react";
+import {useSignIn} from "react-auth-kit";
 
 const SignIn = () => {
+    const [error, setError] = useState("");
+    const signIn = useSignIn();
+    const [passConf, setPassConf] = useState("");
+    const navigator = useNavigate();
+
+    const onSubmit = async (values: RegistrationRequest) => {
+        console.log("Values: ", values);
+        setError("");
+        if (passConf !== values.password) {
+            alert("Passwords do not matches.")
+            return;
+        }
+        try {
+            const response: Response = await fetch("http://localhost:8080/api/v1/auth/register", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            });
+            const responseData: AuthApiResponse = await response.json();
+            console.log(responseData);
+
+            signIn({
+                token: responseData.token,
+                expiresIn: 3600 * 24,
+                tokenType: "Bearer",
+                authState: {email: values.email},
+            });
+            navigator('/account');
+        } catch (err) {
+            console.log("Error: ", err);
+        }
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            email: "",
+            password: ""
+        },
+        onSubmit,
+    });
+
     return (
         <div className="login-container">
             <LogRegWithSocial/>
@@ -15,17 +65,41 @@ const SignIn = () => {
                         Login page</NavLink></p>
                 </div>
 
-                <div className="auth-form inputs-form">
-                    <input placeholder="Username"/>
-                    <input placeholder="Email"/>
-                    <p hidden></p>
-                    <input placeholder="Password"/>
-                    <input placeholder="Password confirmation"/>
-                    <p hidden></p>
-                </div>
-                <div className="auth-form button-form">
-                    <Button class="btn btn-sizing btn-big" text="Submit" onClick={() => {}}/>
-                </div>
+                <form className="auth-form inputs-form"
+                      onSubmit={formik.handleSubmit}>
+                    <input
+                        id="username"
+                        type="text"
+                        placeholder="Enter Username"
+                        value={formik.values.username}
+                        onChange={formik.handleChange}
+                    />
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Enter email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                    />
+                    <input
+                        id="password"
+                        type="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        placeholder="Enter password"
+                    />
+                    <input
+                        id="confpassword"
+                        type="password"
+                        value={passConf}
+                        onChange={e => setPassConf(e.target.value)}
+                        placeholder="Password Confirmation"
+                    />
+                    <div className="auth-form">
+                        <Button class="btn btn-sizing btn-big" text="Submit" onClick={() => {
+                        }}/>
+                    </div>
+                </form>
             </div>
         </div>
     );
